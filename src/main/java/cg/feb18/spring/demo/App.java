3b8c454bac75c5272d43d1d1348ab89a36acf728
phaneendra-web <phanindraa7@gmail.com>
2022-03-23 12:27:20 +0530

    Update App.java

--- src/main/java/cg/feb18/spring/demo/App.java
+++ src/main/java/cg/feb18/spring/demo/App.java
@@ -1,13 +1,40 @@
 package cg.feb18.spring.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cg.feb18.spring.demo.model.Employee;
+
 /**
- * Hello world!
+ * 
+ * @Spring Demos
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+// Bean = object 
+// IoC = Inversion of Control 
+// DI = Dependency Injection 
+// Container = bean container 
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+
+		// pure Java approach
+//		Employee emp = new Employee(101, "Sonu", 50000); // 4 , 8, 8 = 20 100 2000
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+		Employee emp = context.getBean(Employee.class);
+
+		System.out.println(emp.toString());
+
+		System.out.println("End");
+
+		((AbstractApplicationContext) context).close();
+
+	}
 }
